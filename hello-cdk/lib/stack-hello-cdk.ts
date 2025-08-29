e9e6163b710d1726f66226565c084670ee4ffe5c
junh-ki <kijoonh91@gmail.com>
2025-08-29 22:32:35 +0900

    Add the S3 Bucket resource to the HelloCdk stack and grant the access permissions to it to the lambda resource

--- hello-cdk/lib/stack-hello-cdk.ts
+++ hello-cdk/lib/stack-hello-cdk.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { aws_lambda as lambda } from 'aws-cdk-lib';
+import { aws_lambda as lambda, aws_s3 as s3 } from 'aws-cdk-lib';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as path from 'path';
 
@@ -7,10 +7,17 @@ export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new NodejsFunction(this, 'HelloCdkLambda', {
+    const helloCdkLambda = new NodejsFunction(this, 'HelloCdkLambda', {
       entry: path.join(__dirname, './lambda/lambda-hello-cdk/index.ts'),
       handler: 'main', // function name you exported
       runtime: lambda.Runtime.NODEJS_22_X,
     });
+
+    const helloBucket = new s3.Bucket(this, 'HelloCdkS3Bucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    helloBucket.grantReadWrite(helloCdkLambda);
   }
 }
