270f8108f91c31a2c1c01dde0856873a99b13ed3
junh-ki <kijoonh91@gmail.com>
2025-09-02 23:08:14 +0900

    Build the S3 bucket with the L1 construct

--- hello-cdk/lib/stack-hello-cdk.ts
+++ hello-cdk/lib/stack-hello-cdk.ts
@@ -1,23 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
-import { aws_lambda as lambda, aws_s3 as s3 } from 'aws-cdk-lib';
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { CfnBucket } from 'aws-cdk-lib/aws-s3';
 import * as path from 'path';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const helloCdkLambda = new NodejsFunction(this, 'HelloCdkLambda', {
-      entry: path.join(__dirname, './lambda/lambda-hello-cdk/index.ts'),
-      handler: 'main', // function name you exported
-      runtime: lambda.Runtime.NODEJS_22_X,
+    const rawDataBucket = new CfnBucket(this, 'rawDataBucket', {
+      bucketName: 'raw-data-landing-zone-greeting-l1',
+      accessControl: 'Private'
     });
-
-    const helloBucket = new s3.Bucket(this, 'HelloCdkS3Bucket', {
-      versioned: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-    });
-
-    helloBucket.grantReadWrite(helloCdkLambda);
   }
 }
