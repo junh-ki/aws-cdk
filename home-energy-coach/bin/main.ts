c908d295f6fc3361bdb1544428353abac80af84b
junh-ki <kijoonh91@gmail.com>
2025-09-09 11:27:36 +0900

    Restructure the application to have a folder for each stack

--- home-energy-coach/bin/main.ts
+++ home-energy-coach/bin/main.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register'; // To get JavaScript stack traces from TypeScript errors
 import * as cdk from 'aws-cdk-lib';
-import { HelloCdkStack } from '../lib/stacks/stack-data-pipeline';
+import { HelloCdkStack } from '../lib/stacks/stack-data-pipeline/stack-data-pipeline';
 
 const app = new cdk.App();
 new HelloCdkStack(app, 'HelloCdkStack', {
